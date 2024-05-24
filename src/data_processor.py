9e1f7be76d1be8a126fa2547918c9640a73c7c19
Bui Trong Anh <tronganhsm@gmail.com>
2024-05-24 20:07:27 +0700

    docker

--- src/data_processor.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from utils import ratings, parse_json
-
-if __name__ == '__main__':
-    all_ratings = parse_json(ratings.find())
-    df = pd.DataFrame(all_ratings)[['user_id', 'product_id', 'score']].rename(columns={'score':'rating'})
-    df.to_csv('../data/ratings.csv', index=False)
