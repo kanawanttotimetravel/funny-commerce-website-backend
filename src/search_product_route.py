40d55b485f15f1c08d5f4cbae2dfa5cf9f4e6f99
Trọng Anh <110437904+tronganh0104@users.noreply.github.com>
2024-05-25 00:26:06 +0700

    Update search_product_route.py

--- src/search_product_route.py
+++ src/search_product_route.py
@@ -1,21 +1,26 @@
 import os
 from flask import jsonify, request, Blueprint
-import pymongo
+from pymongo import TEXT
 
 from utils import products
 
 search_product_bp = Blueprint('Search-product', __name__)
 
+indexes = products.index_information()
 
+if 'name_text' in indexes:
+    products.drop_index('name_text')
 
-@search_product_bp.route('/Search-product/<string:query>')
+products.create_index([('name', TEXT), ('info', TEXT)])
+
+@search_product_bp.route('/Search-product/<string:query>', methods=['GET'])
 def search_product(query):
     search_results = list(products.find({'$text': {'$search': query}}).limit(20))
 
     if not search_results:
-        return jsonify({'message' : 'No products found'})
+        return jsonify({'message': 'No products found'})
     
     for product in search_results:
         product['_id'] = str(product['_id'])
-    
+
     return jsonify(search_results)
